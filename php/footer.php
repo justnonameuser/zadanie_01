bd89b10cfaeaf6aff2a63a54a4453b7a16039dab
Max <maksimzuh0@gmail.com>
2025-03-14 20:37:03 +0100

    header and footer

---
+++ php/footer.php
@@ -0,0 +1,9 @@
+
+<section class="container-fluid footer_section ">
+    <div class="container">
+      <p>
+        &copy; 2019 All Rights Reserved By
+        <a href="https://html.design/">Free Html Templates</a>
+      </p>
+    </div>
+</section>
